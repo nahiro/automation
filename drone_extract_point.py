41337af5a7ac965f979c50ea543c24781f043a94
Naohiro MANAGO <manago.naohiro@gmail.com>
2022-04-29 03:14:09 +0900

    NM modified files.

--- drone_extract_point.py
+++ drone_extract_point.py
@@ -24,15 +24,17 @@ SN_PARAMS = ['Nr','Br']
 # Default values
 GPS_FNAM = 'gps_points.dat'
 FIGNAM = 'drone_extract_points.pdf'
-RMAX = 0.4 # m
-NMIN = 5
-GROUP_RMAX = 10.0 # m
-GROUP_NMIN = 5
+PIXEL_RMAX = 0.4  # m
+POINT_SMIN = 0.08 # m
+POINT_SMAX = 0.45 # m
+POINT_NMIN = 5
+BUNCH_RMAX = 10.0 # m
+BUNCH_NMIN = 5
 RR_PARAM = 'Lrg'
 SN_PARAM = 'Nr'
-RTHR = 0.1
-STHR = 2.0
+RTHR = 1.0
 RSTP = 0.01
+STHR = 2.0
 
 # Read options
 parser = OptionParser(formatter=IndentedHelpFormatter(max_help_position=200,width=200))
@@ -40,15 +42,17 @@ parser.add_option('-I','--src_geotiff',default=None,help='Source GeoTIFF name (%
 parser.add_option('-O','--dst_geotiff',default=None,help='Destination GeoTIFF name (%default)')
 parser.add_option('-g','--gps_fnam',default=GPS_FNAM,help='GPS file name (%default)')
 parser.add_option('-e','--ext_fnam',default=None,help='Extract file name (%default)')
-parser.add_option('-R','--rmax',default=RMAX,type='float',help='Maximum distance in m (%default)')
-parser.add_option('-N','--nmin',default=NMIN,type='int',help='Minimum number (%default)')
-parser.add_option('--group_rmax',default=GROUP_RMAX,type='float',help='Maximum group distance in m (%default)')
-parser.add_option('--group_nmin',default=GROUP_NMIN,type='int',help='Minimum group in a plot (%default)')
+parser.add_option('-R','--pixel_rmax',default=PIXEL_RMAX,type='float',help='Maximum pixel distance of a point in m (%default)')
+parser.add_option('-m','--point_smin',default=POINT_SMIN,type='float',help='Minimum point size in m (%default)')
+parser.add_option('-M','--point_smax',default=POINT_SMAX,type='float',help='Maximum point size in m (%default)')
+parser.add_option('-N','--point_nmin',default=POINT_NMIN,type='int',help='Minimum point number in a plot (%default)')
+parser.add_option('--bunch_rmax',default=BUNCH_RMAX,type='float',help='Maximum bunch distance in a plot in m (%default)')
+parser.add_option('--bunch_nmin',default=BUNCH_NMIN,type='int',help='Minimum bunch number in a plot (%default)')
 parser.add_option('-p','--rr_param',default=RR_PARAM,help='Redness ratio parameter (%default)')
 parser.add_option('-P','--sn_param',default=SN_PARAM,help='Signal ratio parameter (%default)')
-parser.add_option('-t','--rthr',default=RTHR,type='float',help='Threshold of redness ratio (%default)')
-parser.add_option('-T','--sthr',default=STHR,type='float',help='Threshold of signal ratio (%default)')
+parser.add_option('-t','--rthr',default=RTHR,type='float',help='Max threshold of redness ratio (%default)')
 parser.add_option('-r','--rstp',default=RSTP,type='float',help='Threshold step of redness ratio (%default)')
+parser.add_option('-T','--sthr',default=STHR,type='float',help='Threshold of signal ratio (%default)')
 parser.add_option('-F','--fignam',default=FIGNAM,help='Output figure name (%default)')
 parser.add_option('-z','--ax1_zmin',default=None,type='float',help='Axis1 Z min for debug (%default)')
 parser.add_option('-Z','--ax1_zmax',default=None,type='float',help='Axis1 Z max for debug (%default)')
@@ -78,36 +82,37 @@ with open(opts.gps_fnam,'r') as fp:
         x_bunch.append(float(item[2]))
         y_bunch.append(float(item[3]))
 number_bunch = np.array(number_bunch)
+indx_bunch = np.arange(len(number_bunch))
 plot_bunch = np.array(plot_bunch)
 x_bunch = np.array(x_bunch)
 y_bunch = np.array(y_bunch)
-index_bunch = np.arange(len(number_bunch))
 
 plots = np.unique(plot_bunch)
-groups = {}
-groups_removed = {}
+size_plot = {}
+indx_plot = {}
+removed_plot = {}
 for plot in plots:
     cnd = (plot_bunch == plot)
-    if cnd.sum() < opts.group_nmin:
-        raise ValueError('Error, plot={}, cnd.sum()={} >>> {}'.format(plot,cnd.sum(),opts.gps_fnam))
-    indx = index_bunch[cnd]
-    ng = len(indx)
+    indx = indx_bunch[cnd]
+    size_plot[plot] = len(indx)
+    if size_plot[plot] < opts.bunch_nmin:
+        raise ValueError('Error, plot={}, size_plot[{}]={} >>> {}'.format(plot,plot,size_plot[plot],opts.gps_fnam))
     xg = x_bunch[indx]
     yg = y_bunch[indx]
-    index_member = np.arange(ng)
+    indx_member = np.arange(size_plot[plot])
     flag = []
-    for i_temp in index_member:
-        cnd = (index_member != i_temp)
+    for i_temp in indx_member:
+        cnd = (indx_member != i_temp)
         r = np.sqrt(np.square(xg[cnd]-xg[i_temp])+np.square(yg[cnd]-yg[i_temp]))
-        if r.min() > opts.group_rmax:
+        if r.min() > opts.bunch_rmax:
             flag.append(False)
         else:
             flag.append(True)
     flag = np.array(flag)
-    groups[plot] = indx[flag]
-    groups_removed[plot] = indx[~flag]
-    if len(groups[plot]) < opts.group_nmin:
-        raise ValueError('Error, plot={}, len(groups[{}])={} >>> {}'.format(plot,len(groups[plot]),opts.gps_fnam))
+    indx_plot[plot] = indx[flag]
+    removed_plot[plot] = indx[~flag]
+    if len(indx_plot[plot]) < opts.bunch_nmin:
+        raise ValueError('Error, plot={}, len(indx_plot[{}])={} >>> {}'.format(plot,plot,len(indx_plot[plot]),opts.gps_fnam))
 
 if opts.debug:
     plt.interactive(True)
@@ -153,9 +158,10 @@ for plot in plots:
     if sn_shape != rr_shape:
         raise ValueError('Error, sn_shape={}, rr_shape={} >>> {}'.format(sn_shape,rr_shape,onam))
     sn = ds.ReadAsArray().reshape(sn_ny,sn_nx)
+    cnd_sn = (sn > opts.sthr)
     # Fit line
-    xg_bunch = x_bunch[groups[plot]]
-    yg_bunch = y_bunch[groups[plot]]
+    xg_bunch = x_bunch[indx_plot[plot]]
+    yg_bunch = y_bunch[indx_plot[plot]]
     xc_bunch = xg_bunch.copy()
     yc_bunch = yg_bunch.copy()
     coef = np.polyfit(xc_bunch,yc_bunch,1)
@@ -183,39 +189,137 @@ for plot in plots:
     xd_bunch *= norm
     yd_bunch *= norm
     # Search points
-    rr_point = rr.copy()
-    cnd = (rr < opts.rthr) | (sn < opts.sthr)
-    rr_point[cnd] = np.nan
-    print('HERE, cnd.sum={}'.format(cnd.sum()))
-
-
-    rr_label = label(~cnd)
-    rr_region = regionprops(rr_label)
-    print(len(rr_region))
-
-    """
-    point_xp = rr_xp[~cnd]
-    point_yp = rr_yp[~cnd]
-    points = []
-    for xtmp,ytmp in zip(point_xp,point_yp):
-        flag = False
-        for point in points:
-            for x_member,y_member in point:
-                r = np.sqrt(np.square(xtmp-x_member)+np.square(ytmp-y_member))
-                if r < opts.rmax:
-                    point.append((xtmp,ytmp))
-                    flag = True
+    rr_copy = rr.copy()
+    rthr = opts.rthr
+    while True:
+        cnd = cnd_sn & (rr_copy > rthr)
+        rthr -= opts.rstp
+        labels,num = label(cnd,return_num=True)
+        if num < opts.point_nmin:
+            continue
+        number_point = []
+        xmin_point = []
+        xmax_point = []
+        ymin_point = []
+        ymax_point = []
+        x_point = []
+        y_point = []
+        for region in regionprops(labels):
+            minr,minc,maxr,maxc = region.bbox
+            xmin = rr_xmin+(minc+0.5)*rr_xstp
+            xmax = rr_xmin+(maxc-0.5)*rr_xstp
+            ymax = rr_ymax+(minr+0.5)*rr_ystp
+            ymin = rr_ymax+(maxr-0.5)*rr_ystp
+            indy = region.coords[:,0]
+            indx = region.coords[:,1]
+            x = rr_xmin+(indx+0.5)*rr_xstp
+            y = rr_ymax+(indy+0.5)*rr_ystp
+            flag = False
+            for i_point in range(len(number_point)):
+                if (xmax < xmin_point[i_point]) or (xmin > xmax_point[i_point]) or (ymax < ymin_point[i_point]) or (ymin > ymax_point[i_point]):
+                    continue
+                for xtmp,ytmp in zip(x,y):
+                    r = np.sqrt(np.square(xtmp-x_point[i_point])+np.square(ytmp-y_point[i_point]))
+                    if r.min() < opts.pixel_rmax:
+                        number_point[i_point] += region.area
+                        xmin_point[i_point] = min(xmin_point[i_point],xmin)
+                        xmax_point[i_point] = max(xmax_point[i_point],xmax)
+                        ymin_point[i_point] = min(ymin_point[i_point],ymin)
+                        ymax_point[i_point] = max(ymax_point[i_point],ymax)
+                        x_point[i_point] = np.append(x_point[i_point],x)
+                        y_point[i_point] = np.append(y_point[i_point],y)
+                        flag = True
+                        break
+                if flag:
                     break
-            if flag:
-                break
-        if not flag:
-            points.append([(xtmp,ytmp)])
-    for p in points:
-        if len(p) < opts.nmin:
-            points.remove(p)
-    """
+            if not flag:
+                number_point.append(region.area)
+                xmin_point.append(xmin)
+                xmax_point.append(xmax)
+                ymin_point.append(ymin)
+                ymax_point.append(ymax)
+                x_point.append(x)
+                y_point.append(y)
+        num = len(number_point)
+        if num < opts.point_nmin:
+            continue
+        indexes_to_be_removed = []
+        for i_point in range(len(number_point)):
+            xwid = xmax_point[i_point]-xmin_point[i_point]
+            ywid = ymax_point[i_point]-ymin_point[i_point]
+            if (xwid < opts.point_smin) or (xwid > opts.point_smax) or (ywid < opts.point_smin) or (ywid > opts.point_smax):
+                indexes_to_be_removed.append(i_point)
+        for indx in sorted(indexes_to_be_removed,reverse=True):
+            del number_point[indx]
+            del xmin_point[indx]
+            del xmax_point[indx]
+            del ymin_point[indx]
+            del ymax_point[indx]
+            del x_point[indx]
+            del y_point[indx]
+        num = len(number_point)
+        if num < opts.point_nmin:
+            continue
+        xctr_point = np.array([x.mean() for x in x_point])
+        yctr_point = np.array([y.mean() for y in y_point])
+        xc_point = xctr_point.copy()
+        yc_point = yctr_point.copy()
+        coef = np.polyfit(xc_point,yc_point,1)
+        dist = np.abs(coef[0]*xc_point-yc_point+coef[1])/np.sqrt(coef[0]*coef[0]+1)
+        cnd = np.abs(dist-dist.mean()) < 2.0*dist.std()
+        if not np.all(cnd):
+            xc_point = xc_point[cnd]
+            yc_point = yc_point[cnd]
+            coef = np.polyfit(xc_point,yc_point,1)
+            dist = np.abs(coef[0]*xc_point-yc_point+coef[1])/np.sqrt(coef[0]*coef[0]+1)
+            cnd = np.abs(dist-dist.mean()) < 2.0*dist.std()
+            if not np.all(cnd):
+                xc_point = xc_point[cnd]
+                yc_point = yc_point[cnd]
+                coef = np.polyfit(xc_point,yc_point,1)
+        xf_point = rr_xgrd.copy()
+        yf_point = np.polyval(coef,xf_point)
+        # Origin
+        xo_point = xf_point[0]
+        yo_point = yf_point[0]
+        # Unit direction vector
+        xd_point = xf_point[-1]-xo_point
+        yd_point = yf_point[-1]-yo_point
+        norm = 1.0/np.sqrt(xd_point*xd_point+yd_point*yd_point)
+        xd_point *= norm
+        yd_point *= norm
+        if (xd_bunch*xd_point+yd_bunch*yd_point) < 0.0:
+            xo_point = xf_point[-1]
+            yo_point = yf_point[-1]
+            xd_point *= -1
+            yd_point *= -1
 
 
+
+        break
+    rr_copy = rr.copy()
+    cnd = cnd_sn & (rr_copy > rthr)
+    rr_copy[~cnd] = np.nan
+    #print('HERE, cnd.sum={}'.format(cnd.sum()))
+
+
+#   points = []
+#   for xtmp,ytmp in zip(point_xp,point_yp):
+#       flag = False
+#       for point in points:
+#           for x_member,y_member in point:
+#               r = np.sqrt(np.square(xtmp-x_member)+np.square(ytmp-y_member))
+#               if r < opts.rmax:
+#                   point.append((xtmp,ytmp))
+#                   flag = True
+#                   break
+#           if flag:
+#               break
+#       if not flag:
+#           points.append([(xtmp,ytmp)])
+#   for p in points:
+#       if len(p) < opts.nmin:
+#           points.remove(p)
 
     if opts.debug:
         fig.clear()
@@ -223,13 +327,13 @@ for plot in plots:
         ax1.set_xticks([])
         ax1.set_yticks([])
         if opts.ax1_zmin is not None and opts.ax1_zmax is not None:
-            im = ax1.imshow(rr_point,extent=(rr_xmin,rr_xmax,rr_ymin,rr_ymax),vmin=opts.ax1_zmin,vmax=opts.ax1_zmax,cmap=cm.jet,interpolation='none')
+            im = ax1.imshow(rr_copy,extent=(rr_xmin,rr_xmax,rr_ymin,rr_ymax),vmin=opts.ax1_zmin,vmax=opts.ax1_zmax,cmap=cm.jet,interpolation='none')
         elif opts.ax1_zmin is not None:
-            im = ax1.imshow(rr_point,extent=(rr_xmin,rr_xmax,rr_ymin,rr_ymax),vmin=opts.ax1_zmin,cmap=cm.jet,interpolation='none')
+            im = ax1.imshow(rr_copy,extent=(rr_xmin,rr_xmax,rr_ymin,rr_ymax),vmin=opts.ax1_zmin,cmap=cm.jet,interpolation='none')
         elif opts.ax1_zmax is not None:
-            im = ax1.imshow(rr_point,extent=(rr_xmin,rr_xmax,rr_ymin,rr_ymax),vmax=opts.ax1_zmax,cmap=cm.jet,interpolation='none')
+            im = ax1.imshow(rr_copy,extent=(rr_xmin,rr_xmax,rr_ymin,rr_ymax),vmax=opts.ax1_zmax,cmap=cm.jet,interpolation='none')
         else:
-            im = ax1.imshow(rr_point,extent=(rr_xmin,rr_xmax,rr_ymin,rr_ymax),cmap=cm.jet,interpolation='none')
+            im = ax1.imshow(rr_copy,extent=(rr_xmin,rr_xmax,rr_ymin,rr_ymax),cmap=cm.jet,interpolation='none')
         divider = make_axes_locatable(ax1)
         cax = divider.append_axes('right',size='5%',pad=0.05)
         if opts.ax1_zstp is not None:
@@ -248,37 +352,13 @@ for plot in plots:
         ax2.set_ylabel(opts.rr_param)
         ax2.yaxis.set_label_coords(3.5,0.5)
 
+        #for i_point in range(len(number_point)):
+        #    rect = mpatches.Rectangle((xmin_point[i_point],ymin_point[i_point]),xmax_point[i_point]-xmin_point[i_point],ymax_point[i_point]-ymin_point[i_point],fill=False,edgecolor='red',linewidth=2)
+            #ax1.add_patch(rect)
 
-
-        for i,region in enumerate(regionprops(rr_label)):
-            # take regions with large enough areas
-            if region.area < 0:
-                raise ValueError('Error, region.area={}'.format(region.area))
-            inds = np.ravel_multi_index((region.coords[:,0],region.coords[:,1]),rr_shape)
-            # draw rectangle around segmented coins
-            minr0,minc0,maxr0,maxc0 = region.bbox
-            minr = rr_yp[minr0,minc0]
-            minc = rr_xp[minr0,minc0]
-            maxr = rr_yp[min(maxr0,rr_ny-1),min(maxc0,rr_nx-1)]
-            maxc = rr_xp[min(maxr0,rr_ny-1),min(maxc0,rr_nx-1)]
-            #minr,minc,maxr,maxc = region.bbox
-            #p1 = (xp[minr,minc],yp[minr,minc])
-            #p2 = (xp[minr,maxc],yp[minr,maxc])
-            #p3 = (xp[maxr,maxc],yp[maxr,maxc])
-            #p4 = (xp[maxr,minc],yp[maxr,minc])
-            rect = mpatches.Rectangle((minc,minr),maxc-minc,maxr-minr,fill=False,edgecolor='red',linewidth=2)
-            #rect = mpatches.PathPatch(mpatches.Path([p1,p2,p3,p4,p1]),fill=False,edgecolor='red',linewidth=2)
-            ax1.add_patch(rect)
-            #if region.area > 100:
-            #    #ax1.text(0.5*(minc+maxc),0.5*(minr+maxr),'{}'.format(object_id),ha='center',va='center',color='r')
-            #    ax1.text(0.5*(minc+maxc),0.5*(minr+maxr),'{}'.format(object_id),ha='center',va='center',color='r')
-
-
-
-
-        #ax1.plot(xg_bunch,yg_bunch,'o',mfc='none',mec='k')
-        #ax1.plot(xf_bunch,yf_bunch,'k:')
-        #ng = number_bunch[groups[plot]]
+        ax1.plot(xctr_point,yctr_point,'o',mfc='none',mec='k')
+        ax1.plot(xf_point,yf_point,'k:')
+        #ng = number_bunch[indx_plot[plot]]
         #for ntmp,xtmp,ytmp in zip(ng,xg_bunch,yg_bunch):
         #    ax1.text(xtmp,ytmp,'{}'.format(ntmp))
         if opts.remove_nan:
