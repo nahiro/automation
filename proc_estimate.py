196ea9cf9276caad0f3c633bcd5a02bf74a9ab03
Naohiro Manago <manago.naohiro@gmail.com>
2022-05-31 09:52:39 +0900

    NM modified files.

--- proc_estimate.py
+++ proc_estimate.py
@@ -10,6 +10,7 @@ proc_estimate.pnams.append('intensity_fnam')
 proc_estimate.pnams.append('intensity_number')
 proc_estimate.pnams.append('y_params')
 proc_estimate.pnams.append('score_max')
+proc_estimate.pnams.append('score_step')
 proc_estimate.pnams.append('gis_fnam')
 proc_estimate.pnams.append('buffer')
 proc_estimate.pnams.append('region_size')
@@ -20,6 +21,7 @@ proc_estimate.params['intensity_fnam'] = 'Intensity Formula File'
 proc_estimate.params['intensity_number'] = 'Intensity Formula Number'
 proc_estimate.params['y_params'] = 'Output Variable'
 proc_estimate.params['score_max'] = 'Max Output Score'
+proc_estimate.params['score_step'] = 'Digitize Score Step'
 proc_estimate.params['gis_fnam'] = 'Polygon File'
 proc_estimate.params['buffer'] = 'Buffer Radius (m)'
 proc_estimate.params['region_size'] = 'Average Region Size (m)'
@@ -30,12 +32,14 @@ proc_estimate.param_types['intensity_fnam'] = 'string'
 proc_estimate.param_types['intensity_number'] = 'int'
 proc_estimate.param_types['y_params'] = 'boolean_list'
 proc_estimate.param_types['score_max'] = 'int_list'
+proc_estimate.param_types['score_step'] = 'int_list'
 proc_estimate.param_types['gis_fnam'] = 'string'
 proc_estimate.param_types['buffer'] = 'double'
 proc_estimate.param_types['region_size'] = 'double'
 proc_estimate.param_range['score_number'] = (1,10000)
 proc_estimate.param_range['intensity_number'] = (1,10000)
 proc_estimate.param_range['score_max'] = (1,65535)
+proc_estimate.param_range['score_step'] = (1,65535)
 proc_estimate.param_range['buffer'] = (0.0,10.0e3)
 proc_estimate.param_range['region_size'] = (0.0,1.0e3)
 proc_estimate.defaults['inp_fnam'] = 'input.tif'
@@ -45,13 +49,16 @@ proc_estimate.defaults['intensity_fnam'] = 'intensity_formula.csv'
 proc_estimate.defaults['intensity_number'] = 1
 proc_estimate.defaults['y_params'] = [True,False,False,False,False,False]
 proc_estimate.defaults['score_max'] = [9,9,1,1,1,9]
+proc_estimate.defaults['score_step'] = [2,2,1,1,1,2]
 proc_estimate.defaults['gis_fnam'] = 'All_area_polygon_20210914.shp'
 proc_estimate.defaults['buffer'] = 1.0
 proc_estimate.defaults['region_size'] = 1.0
 proc_estimate.list_sizes['y_params'] = 6
 proc_estimate.list_sizes['score_max'] = 6
+proc_estimate.list_sizes['score_step'] = 6
 proc_estimate.list_labels['y_params'] = ['BLB','Blast','StemBorer','Rat','Hopper','Drought']
 proc_estimate.list_labels['score_max'] = ['BLB :',' Blast :',' StemBorer :',' Rat :',' Hopper :',' Drought :']
+proc_estimate.list_labels['score_step'] = ['BLB :',' Blast :',' StemBorer :',' Rat :',' Hopper :',' Drought :']
 proc_estimate.input_types['inp_fnam'] = 'ask_file'
 proc_estimate.input_types['score_fnam'] = 'ask_file'
 proc_estimate.input_types['score_number'] = 'box'
@@ -59,6 +66,7 @@ proc_estimate.input_types['intensity_fnam'] = 'ask_file'
 proc_estimate.input_types['intensity_number'] = 'box'
 proc_estimate.input_types['y_params'] = 'boolean_list'
 proc_estimate.input_types['score_max'] = 'int_list'
+proc_estimate.input_types['score_step'] = 'int_list'
 proc_estimate.input_types['gis_fnam'] = 'ask_file'
 proc_estimate.input_types['buffer'] = 'box'
 proc_estimate.input_types['region_size'] = 'box'
