0ca11c9d1190a551825a0b922d4cb1f8c47cf24d
Naohiro Manago <manago.naohiro@gmail.com>
2022-06-20 13:51:13 +0900

    NM modified files.

--- run_estimate.py
+++ run_estimate.py
@@ -21,19 +21,14 @@ class Estimate(Process):
         if not os.path.exists(self.values['intensity_fnam']):
             raise IOError('{}: error, no such file >>> {}'.format(self.proc_name,self.values['intensity_fnam']))
         trg_bnam = '{}_{}'.format(self.current_block,self.current_date)
-        etc_dir = os.path.join(self.drone_analysis,self.current_block,self.current_date,self.proc_name)
-        wrk_dir = os.path.join(self.drone_analysis,self.proc_name)
-        if not os.path.exists(etc_dir):
-            os.makedirs(etc_dir)
-        if not os.path.isdir(etc_dir):
-            raise ValueError('{}: error, no such folder >>> {}'.format(self.proc_name,etc_dir))
+        wrk_dir = os.path.join(self.drone_analysis,self.current_block,self.current_date,self.proc_name)
         if not os.path.exists(wrk_dir):
             os.makedirs(wrk_dir)
         if not os.path.isdir(wrk_dir):
             raise ValueError('{}: error, no such folder >>> {}'.format(self.proc_name,wrk_dir))
 
         # Make mask
-        mask_fnam = os.path.join(etc_dir,'{}_mask.tif'.format(trg_bnam))
+        mask_fnam = os.path.join(wrk_dir,'{}_mask.tif'.format(trg_bnam))
         command = self.python_path
         command += ' {}'.format(os.path.join(self.scr_dir,'make_mask.py'))
         command += ' --shp_fnam {}'.format(self.values['gis_fnam'])
@@ -47,7 +42,7 @@ class Estimate(Process):
         call(command,shell=True)
 
         # Make rebinned image
-        img_fnam = os.path.join(etc_dir,'{}_resized.tif'.format(trg_bnam))
+        img_fnam = os.path.join(wrk_dir,'{}_resized.tif'.format(trg_bnam))
         ds = gdal.Open(self.values['inp_fnam'])
         inp_trans = ds.GetGeoTransform()
         inp_shape = (ds.RasterYSize,ds.RasterXSize)
@@ -76,7 +71,7 @@ class Estimate(Process):
         call(command,shell=True)
 
         # Make parcel image
-        mask_resized_fnam = os.path.join(etc_dir,'{}_mask_resized.tif'.format(trg_bnam))
+        mask_resized_fnam = os.path.join(wrk_dir,'{}_mask_resized.tif'.format(trg_bnam))
         command = self.python_path
         command += ' {}'.format(os.path.join(self.scr_dir,'rebin_mask.py'))
         command += ' --istp {}'.format(istp)
@@ -94,7 +89,7 @@ class Estimate(Process):
         command += ' {}'.format(os.path.join(self.scr_dir,'drone_score_estimate.py'))
         command += ' --inp_fnam {}'.format(self.values['score_fnam'])
         command += ' --src_geotiff {}'.format(img_fnam)
-        command += ' --dst_geotiff {}'.format(os.path.join(etc_dir,'{}_score.tif'.format(trg_bnam)))
+        command += ' --dst_geotiff {}'.format(os.path.join(wrk_dir,'{}_score.tif'.format(trg_bnam)))
         for param,flag in zip(self.list_labels['y_params'],self.values['y_params']):
             if flag:
                 command += ' --y_param {}'.format(param)
@@ -107,7 +102,7 @@ class Estimate(Process):
                 command += ' --sint {}'.format(value)
         if self.values['digitize']:
             command += ' --digitize'
-        command += ' --fignam {}'.format(os.path.join(etc_dir,'{}_score.pdf'.format(trg_bnam)))
+        command += ' --fignam {}'.format(os.path.join(wrk_dir,'{}_score.pdf'.format(trg_bnam)))
         command += ' --remove_nan'
         command += ' --debug'
         command += ' --batch'
@@ -121,13 +116,13 @@ class Estimate(Process):
         command += ' {}'.format(os.path.join(self.scr_dir,'drone_score_estimate.py'))
         command += ' --inp_fnam {}'.format(self.values['intensity_fnam'])
         command += ' --src_geotiff {}'.format(img_fnam)
-        command += ' --dst_geotiff {}'.format(os.path.join(etc_dir,'{}_intensity.tif'.format(trg_bnam)))
+        command += ' --dst_geotiff {}'.format(os.path.join(wrk_dir,'{}_intensity.tif'.format(trg_bnam)))
         for param,flag in zip(self.list_labels['y_params'],self.values['y_params']):
             if flag:
                 command += ' --y_param {}'.format(param)
                 command += ' --y_number {}'.format(self.values['intensity_number'])
                 command += ' --smax 1'
-        command += ' --fignam {}'.format(os.path.join(etc_dir,'{}_intensity.pdf'.format(trg_bnam)))
+        command += ' --fignam {}'.format(os.path.join(wrk_dir,'{}_intensity.pdf'.format(trg_bnam)))
         command += ' --remove_nan'
         command += ' --debug'
         command += ' --batch'
@@ -139,11 +134,11 @@ class Estimate(Process):
         # Calculate damage intensity of plot from score
         command = self.python_path
         command += ' {}'.format(os.path.join(self.scr_dir,'drone_damage_calculate.py'))
-        command += ' --src_geotiff {}'.format(os.path.join(etc_dir,'{}_score.tif'.format(trg_bnam)))
+        command += ' --src_geotiff {}'.format(os.path.join(wrk_dir,'{}_score.tif'.format(trg_bnam)))
         command += ' --mask_geotiff {}'.format(mask_resized_fnam)
         command += ' --shp_fnam {}'.format(self.values['gis_fnam'])
         command += ' --out_csv {}'.format(os.path.join(wrk_dir,'{}_damage_score.csv'.format(trg_bnam)))
-        command += ' --out_shp {}'.format(os.path.join(etc_dir,'{}_damage_score.shp'.format(trg_bnam)))
+        command += ' --out_shp {}'.format(os.path.join(wrk_dir,'{}_damage_score.shp'.format(trg_bnam)))
         for param,flag in zip(self.list_labels['y_params'],self.values['y_params']):
             if flag:
                 command += ' --y_param {}'.format(param)
@@ -165,11 +160,11 @@ class Estimate(Process):
         # Calculate damage intensity of plot from intensity
         command = self.python_path
         command += ' {}'.format(os.path.join(self.scr_dir,'drone_damage_calculate.py'))
-        command += ' --src_geotiff {}'.format(os.path.join(etc_dir,'{}_intensity.tif'.format(trg_bnam)))
+        command += ' --src_geotiff {}'.format(os.path.join(wrk_dir,'{}_intensity.tif'.format(trg_bnam)))
         command += ' --mask_geotiff {}'.format(mask_resized_fnam)
         command += ' --shp_fnam {}'.format(self.values['gis_fnam'])
         command += ' --out_csv {}'.format(os.path.join(wrk_dir,'{}_damage_intensity.csv'.format(trg_bnam)))
-        command += ' --out_shp {}'.format(os.path.join(etc_dir,'{}_damage_intensity.shp'.format(trg_bnam)))
+        command += ' --out_shp {}'.format(os.path.join(wrk_dir,'{}_damage_intensity.shp'.format(trg_bnam)))
         for param,flag in zip(self.list_labels['y_params'],self.values['y_params']):
             if flag:
                 command += ' --y_param {}'.format(param)
